4fa063c2181327b4d6996ba7192eb68b87f75809
Selton Guedes <sfgs@ecomp.poli.br>
2023-05-09 16:59:52 -0300

    LPS-179931 Create methods to get object entry values from DTO and a method to get title field value"

--- modules/apps/object/object-api/src/main/java/com/liferay/object/entry/util/ObjectEntryValuesUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/entry/util/ObjectEntryValuesUtil.java
@@ -18,22 +18,104 @@ import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
 import com.liferay.object.constants.ObjectFieldConstants;
 import com.liferay.object.model.ObjectField;
+import com.liferay.object.system.SystemObjectDefinitionManager;
+import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlParserUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.util.ObjectMapperUtil;
 
 import java.io.Serializable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Feliphe Marinho
  */
 public class ObjectEntryValuesUtil {
+
+	public static Map<String, Object> getObjectEntryValues(
+		BaseModel<?> baseModel, DTOConverterRegistry dtoConverterRegistry,
+		String objectDefinitionName,
+		SystemObjectDefinitionManagerRegistry
+			systemObjectDefinitionManagerRegistry,
+		User user) {
+
+		try {
+			Object dtoModel = ObjectEntryDTOConverterUtil.toDTO(
+				baseModel, dtoConverterRegistry,
+				systemObjectDefinitionManagerRegistry.
+					getSystemObjectDefinitionManager(objectDefinitionName),
+				user);
+
+			return ObjectMapperUtil.readValue(Map.class, dtoModel.toString());
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+
+		return new HashMap<>();
+	}
+
+	public static Map<String, Object> getObjectEntryValues(
+			long companyId, DTOConverterRegistry dtoConverterRegistry,
+			String objectDefinitionName, long primaryKey,
+			SystemObjectDefinitionManagerRegistry
+				systemObjectDefinitionManagerRegistry,
+			User user)
+		throws PortalException {
+
+		SystemObjectDefinitionManager systemObjectDefinitionManager =
+			systemObjectDefinitionManagerRegistry.
+				getSystemObjectDefinitionManager(objectDefinitionName);
+
+		return getObjectEntryValues(
+			systemObjectDefinitionManager.getBaseModelByExternalReferenceCode(
+				systemObjectDefinitionManager.getExternalReferenceCode(
+					primaryKey),
+				companyId),
+			dtoConverterRegistry, objectDefinitionName,
+			systemObjectDefinitionManagerRegistry, user);
+	}
+
+	public static Object getTitleFieldValue(
+		String businessType, Object value, String userLanguageId) {
+
+		if (value instanceof Map) {
+			Map<String, Object> localizedValues = (Map<String, Object>)value;
+
+			String siteDefaultLanguageId = String.valueOf(
+				LocaleUtil.getSiteDefault());
+
+			if (localizedValues.containsKey(siteDefaultLanguageId)) {
+				value = localizedValues.get(siteDefaultLanguageId);
+			}
+			else if (localizedValues.containsKey(userLanguageId)) {
+				value = localizedValues.get(userLanguageId);
+			}
+			else {
+				value = localizedValues.get(
+					String.valueOf(LocaleUtil.getDefault()));
+			}
+		}
+		else if (StringUtil.equals(
+					businessType, ObjectFieldConstants.BUSINESS_TYPE_BOOLEAN)) {
+
+			value = Boolean.parseBoolean(String.valueOf(value));
+		}
+
+		return value;
+	}
 
 	public static String getValueString(
 		ObjectField objectField, Map<String, Serializable> values) {
