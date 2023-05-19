0766a1ce1bc962e89b01fe111d617a6f1e2bc432
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-05-19 08:29:05 -0300

    LPS-183396 Fix tests

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-test/src/testIntegration/java/com/liferay/site/initializer/extender/internal/test/BundleSiteInitializerTest.java
@@ -1892,10 +1892,12 @@ public class BundleSiteInitializerTest {
 			_objectDefinitionLocalService.fetchObjectDefinition(
 				_group.getCompanyId(), "C_TestObjectDefinition1");
 
-		Assert.assertFalse(objectDefinition1.isAccountEntryRestricted());
+		Assert.assertTrue(objectDefinition1.isAccountEntryRestricted());
 		Assert.assertFalse(objectDefinition1.isSystem());
 		Assert.assertEquals(
 			objectDefinition1.getStatus(), WorkflowConstants.STATUS_APPROVED);
+		Assert.assertEquals(objectDefinition1.getStorageType(),
+			"salesforce");
 
 		_assertObjectActions(3, objectDefinition1);
 		_assertObjectEntries(_group.getGroupId(), objectDefinition1, 0);
@@ -1949,6 +1951,8 @@ public class BundleSiteInitializerTest {
 		Assert.assertFalse(objectDefinition4.isSystem());
 		Assert.assertEquals(
 			objectDefinition4.getStatus(), WorkflowConstants.STATUS_APPROVED);
+		Assert.assertEquals(objectDefinition4.getStorageType(),
+			"default");
 
 		ObjectDefinition objectDefinition5 =
 			_objectDefinitionLocalService.fetchObjectDefinition(
@@ -1961,6 +1965,8 @@ public class BundleSiteInitializerTest {
 
 		Assert.assertEquals(
 			objectDefinition4.getStatus(), WorkflowConstants.STATUS_APPROVED);
+		Assert.assertEquals(objectDefinition5.getStorageType(),
+			"default");
 	}
 
 	private void _assertObjectEntries(
