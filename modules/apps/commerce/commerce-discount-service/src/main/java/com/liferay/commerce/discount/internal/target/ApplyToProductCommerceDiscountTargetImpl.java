ef167a92db8194cc53dd852dc2fd797e79341c88
Joao Victor <joao.victor@liferay.com>
2023-05-26 17:02:33 -0300

    LPS-185842 Extract CommerceDiscountProductTarget methods from ApplyToProductCommerceDiscountTargetImpl to ApplyToProductCommerceDiscountProductTarget

--- modules/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/internal/target/ApplyToProductCommerceDiscountTargetImpl.java
+++ modules/apps/commerce/commerce-discount-service/src/main/java/com/liferay/commerce/discount/internal/target/ApplyToProductCommerceDiscountTargetImpl.java
@@ -15,20 +15,8 @@
 package com.liferay.commerce.discount.internal.target;
 
 import com.liferay.commerce.discount.constants.CommerceDiscountConstants;
-import com.liferay.commerce.discount.model.CommerceDiscount;
-import com.liferay.commerce.discount.model.CommerceDiscountRel;
-import com.liferay.commerce.discount.service.CommerceDiscountRelLocalService;
-import com.liferay.commerce.discount.target.CommerceDiscountProductTarget;
 import com.liferay.commerce.discount.target.CommerceDiscountTarget;
-import com.liferay.commerce.product.model.CPDefinition;
-import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.search.BooleanClauseOccur;
-import com.liferay.portal.kernel.search.Document;
-import com.liferay.portal.kernel.search.filter.BooleanFilter;
-import com.liferay.portal.kernel.search.filter.ExistsFilter;
-import com.liferay.portal.kernel.search.filter.Filter;
-import com.liferay.portal.kernel.search.filter.TermFilter;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.util.Locale;
@@ -46,25 +34,10 @@ import org.osgi.service.component.annotations.Reference;
 		"commerce.discount.target.key=" + CommerceDiscountConstants.TARGET_PRODUCTS,
 		"commerce.discount.target.order:Integer=20"
 	},
-	service = {
-		CommerceDiscountProductTarget.class, CommerceDiscountTarget.class
-	}
+	service = CommerceDiscountTarget.class
 )
 public class ApplyToProductCommerceDiscountTargetImpl
-	implements CommerceDiscountProductTarget, CommerceDiscountTarget {
-
-	@Override
-	public void contributeDocument(
-		Document document, CommerceDiscount commerceDiscount) {
-
-		document.addKeyword(
-			"commerce_discount_target_cp_definition_ids",
-			TransformUtil.transformToLongArray(
-				_commerceDiscountRelLocalService.getCommerceDiscountRels(
-					commerceDiscount.getCommerceDiscountId(),
-					CPDefinition.class.getName()),
-				CommerceDiscountRel::getClassPK));
-	}
+	implements CommerceDiscountTarget {
 
 	@Override
 	public String getKey() {
@@ -83,32 +56,6 @@ public class ApplyToProductCommerceDiscountTargetImpl
 	public Type getType() {
 		return Type.APPLY_TO_PRODUCT;
 	}
-
-	@Override
-	public void postProcessContextBooleanFilter(
-		BooleanFilter contextBooleanFilter, CPDefinition cpDefinition) {
-
-		TermFilter termFilter = new TermFilter(
-			"commerce_discount_target_cp_definition_ids",
-			String.valueOf(cpDefinition.getCPDefinitionId()));
-
-		Filter existFilter = new ExistsFilter(
-			"commerce_discount_target_cp_definition_ids");
-
-		BooleanFilter existBooleanFilter = new BooleanFilter();
-
-		existBooleanFilter.add(existFilter, BooleanClauseOccur.MUST_NOT);
-
-		BooleanFilter fieldBooleanFilter = new BooleanFilter();
-
-		fieldBooleanFilter.add(existBooleanFilter, BooleanClauseOccur.SHOULD);
-		fieldBooleanFilter.add(termFilter, BooleanClauseOccur.SHOULD);
-
-		contextBooleanFilter.add(fieldBooleanFilter, BooleanClauseOccur.MUST);
-	}
-
-	@Reference
-	private CommerceDiscountRelLocalService _commerceDiscountRelLocalService;
 
 	@Reference
 	private Language _language;
