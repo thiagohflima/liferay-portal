4e267472a2bf84d996012947886d1e965aecad1a
Qi Zhang <qi.zhang@liferay.com>
2023-05-31 11:21:14 +0800

    LPS-186127 Add testcase

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/PropertiesSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/PropertiesSourceProcessorTest.java
@@ -27,6 +27,39 @@ public class PropertiesSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testLanguageKeyContext() throws Exception {
+		test(SourceProcessorTestParameters
+			.create("LanguageKeysContext.testproperties")
+			.addExpectedMessage(
+				"The Key 'a' should with [context] or a context suffix to indicate specific meaning"
+			).addExpectedMessage(
+				"The Key 'add' should with [context] or a context suffix to indicate specific meaning"
+			).addExpectedMessage(
+				"The Key 'alert' should with [context] or a context suffix to indicate specific meaning"
+			).addExpectedMessage(
+				"The Key 'answer[n]' contain unqualified context for 'n'"
+			).addExpectedMessage(
+				"The Key 'average' should with [context] or a context suffix to indicate specific meaning"
+			).addExpectedMessage(
+				"The Key 'order' should with [context] or a context suffix to indicate specific meaning"
+			).addExpectedMessage(
+				"The Key 'order[...]' contain unqualified context for '...'"
+			).addExpectedMessage(
+				"The Key 'order[0]' contain unqualified context for '0'"
+			).addExpectedMessage(
+				"The Key 'order[123]' contain unqualified context for '123'"
+			).addExpectedMessage(
+				"The Key 'order[]' contain unqualified context for ''"
+			).addExpectedMessage(
+				"The Key 'order[abc]' contain unqualified context for 'abc'"
+			).addExpectedMessage(
+				"The Key 'order[x]' contain unqualified context for 'x'"
+			).addExpectedMessage(
+				"The Key 'order[xyz]' contain unqualified context for 'xyz'"
+			));
+	}
+
+	@Test
 	public void testSortDefinitionKeys() throws Exception {
 		test("FormatProperties/liferay-plugin-package.testproperties");
 		test("FormatProperties/TLiferayBatchFileProperties.testproperties");
