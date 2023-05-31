2886800aa746c6c79b1c737b228e32e2ad5096e7
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-05-31 11:10:33 +0800

    LPS-171023 Add a new constant class WorkflowMetricsIndexEntityNameConstants, prepara for next

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/constants/WorkflowMetricsIndexEntityNameConstants.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.portal.workflow.metrics.search.index.constants;
+
+/**
+ * @author Jiaxu Wei
+ */
+public interface WorkflowMetricsIndexEntityNameConstants {
+
+	public static final String SUFFIX_INSTANCE = "-workflow-metrics-instances";
+
+	public static final String SUFFIX_NODE = "-workflow-metrics-nodes";
+
+	public static final String SUFFIX_PROCESS = "-workflow-metrics-processes";
+
+	public static final String SUFFIX_SLA_INSTANCE_RESULT =
+		"-workflow-metrics-sla-instance-results";
+
+	public static final String SUFFIX_SLA_TASK_RESULT =
+		"-workflow-metrics-sla-task-results";
+
+	public static final String SUFFIX_TASK = "-workflow-metrics-tasks";
+
+	public static final String SUFFIX_TRANSITION =
+		"-workflow-metrics-transitions";
+
+}
