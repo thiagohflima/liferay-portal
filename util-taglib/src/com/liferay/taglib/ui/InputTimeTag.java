ac49f188edbea04909b4ff80d322acc5eefa30e1
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-05-31 14:22:00 +0200

    LPS-169277 Deprecate the minuteInterval attribute because it is currently not used

--- util-taglib/src/com/liferay/taglib/ui/InputTimeTag.java
+++ util-taglib/src/com/liferay/taglib/ui/InputTimeTag.java
@@ -117,6 +117,10 @@ public class InputTimeTag extends IncludeTag {
 		_hourValue = hourValue;
 	}
 
+	/**
+	 * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
+	 */
+	@Deprecated
 	public void setMinuteInterval(int minuteInterval) {
 		_minuteInterval = minuteInterval;
 	}
