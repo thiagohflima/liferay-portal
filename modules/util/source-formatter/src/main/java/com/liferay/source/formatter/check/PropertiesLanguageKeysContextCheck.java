c2baa97b7834e99f387f791743b1305c95dd8648
Alan Huang <alan.huang@liferay.com>
2023-06-07 14:34:11 +0800

    LPS-186127 SF

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesLanguageKeysContextCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/PropertiesLanguageKeysContextCheck.java
@@ -16,7 +16,6 @@ package com.liferay.source.formatter.check;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -83,10 +82,8 @@ public class PropertiesLanguageKeysContextCheck extends BaseFileCheck {
 				((bracketsContent.length() == 1) &&
 				 !bracketsContent.equals("n") &&
 				 !bracketsContent.equals("v")) ||
-				(bracketsContent.matches("\\d+") &&
-				 !ArrayUtil.contains(
-					 _HTTP_STATUS_CODE,
-					 GetterUtil.getInteger(bracketsContent))) ||
+				(bracketsContent.matches("\\d+") && !key.contains("code") &&
+				 !key.contains("status")) ||
 				getAttributeValues(
 					_FORBIDDEN_CONTEXT_NAMES_KEY, absolutePath
 				).contains(
@@ -108,13 +105,6 @@ public class PropertiesLanguageKeysContextCheck extends BaseFileCheck {
 
 	private static final String _FORBIDDEN_CONTEXT_NAMES_KEY =
 		"forbiddenContextNames";
-
-	private static final int[] _HTTP_STATUS_CODE = {
-		100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 301, 302, 303,
-		304, 305, 307, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411,
-		412, 413, 414, 415, 416, 417, 419, 420, 422, 423, 424, 429, 500, 501,
-		502, 503, 504, 505, 507
-	};
 
 	private static final Pattern _languageKeyPattern = Pattern.compile(
 		"([\\s\\S]+)(\\[([\\s\\S]*)\\])");
