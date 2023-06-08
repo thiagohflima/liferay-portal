d1f1293d46075e08f784bc4979951cda2ba593de
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-06-08 12:06:52 -0300

    LPS-185518 Create UpgradeJavaSetResultsSetTotalMethodCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeJavaSetResultsSetTotalMethodCheck.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Tamyris Bernardo
+ */
+public class UpgradeSetResultsSetTotalMethodCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws Exception {
+
+		if (!fileName.endsWith(".java") && !fileName.endsWith(".jsp") &&
+			!fileName.endsWith(".jspf")) {
+
+			return content;
+		}
+
+		content = _removeSetTotal(content);
+		content = _replaceSetResults(content);
+
+		return content;
+	}
+
+	private String _removeSetTotal(String content) {
+		String newContent = content;
+		Matcher setTotalMatcher = _setTotalPattern.matcher(content);
+
+		while (setTotalMatcher.find()) {
+			String methodCall = JavaSourceUtil.getMethodCall(
+				content, setTotalMatcher.start());
+
+			String fullMethodCall = StringBundler.concat(
+				StringPool.NEW_LINE, methodCall, StringPool.SEMICOLON,
+				StringPool.NEW_LINE);
+
+			if (hasClassOrVariableName(
+					"SearchContainer", content, methodCall.trim())) {
+
+				newContent = StringUtil.removeSubstring(
+					newContent, fullMethodCall);
+			}
+		}
+
+		return newContent;
+	}
+
+	private String _replaceSetResults(String content) {
+		String newContent = content;
+		Matcher setResultsMatcher = _setResultsPattern.matcher(content);
+
+		while (setResultsMatcher.find()) {
+			String methodCall = JavaSourceUtil.getMethodCall(
+				content, setResultsMatcher.start());
+
+			if (hasClassOrVariableName(
+					"SearchContainer", content, methodCall.trim())) {
+
+				newContent = StringUtil.replace(
+					newContent, methodCall,
+					StringUtil.replace(
+						methodCall, ".setResults(", ".setResultsAndTotal("));
+			}
+		}
+
+		return newContent;
+	}
+
+	private static final Pattern _setResultsPattern = Pattern.compile(
+		"\\w+\\.setResults\\(");
+	private static final Pattern _setTotalPattern = Pattern.compile(
+		"\\t*?\\w+\\.setTotal\\(");
+
+}
