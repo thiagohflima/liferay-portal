42a220e8efe45b5922f4a8ac31aeb6fbfeabd482
Elias Santos <elias.santos@liferay.com>
2023-06-09 10:51:03 -0300

    LPS-187164 Remove groovy script from payment confirmation

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-save-button/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-request-save-button/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable @liferay/portal/no-global-fetch */
-/* eslint-disable no-undef */
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
@@ -14,18 +12,43 @@
 const queryString = window.location.search;
 const urlParams = queryString.split('=');
 const requestId = urlParams[1];
+const liferayUrl = window.location.origin;
 const requestIdInput = document.querySelector('.request-id div input');
 const requestIdValueField = document.getElementsByName(
 	'r_requestId_c_evpRequestId'
 )[0];
 const selectButton = document.querySelector('.btn-secondary');
-
 requestIdInput.disabled = true;
-
 requestIdInput.style.borderRadius = '8px';
-
 requestIdInput.value = requestId;
-
 requestIdValueField.value = requestId;
 
 selectButton.remove();
+
+const statusResponse = async () => {
+	const payload = {
+		requestStatus: {
+			key: 'closed',
+			name: 'Closed',
+		},
+	};
+
+	// eslint-disable-next-line @liferay/portal/no-global-fetch
+	await fetch(`${liferayUrl}/o/c/evprequests/${requestId}`, {
+		body: JSON.stringify(payload),
+		headers: {
+			'content-type': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+		method: 'PATCH',
+	});
+};
+
+// eslint-disable-next-line no-undef
+const formElement = fragmentElement.querySelector(
+	'.lfr-layout-structure-item-form'
+);
+
+if (formElement) {
+	formElement.onsubmit = () => statusResponse();
+}
