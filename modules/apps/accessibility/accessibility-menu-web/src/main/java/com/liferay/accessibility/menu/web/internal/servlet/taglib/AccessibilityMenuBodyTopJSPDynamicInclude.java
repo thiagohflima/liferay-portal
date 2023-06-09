4322fa83df3972e5f9f875a03431f7bfa7c20541
Evan Thibodeau <evan.thibodeau@liferay.com>
2023-06-10 00:06:22 +0400

    LPS-187203 - Use DynamicInclude to always render the Accessibility Menu Portlet on every page.

---
+++ modules/apps/accessibility/accessibility-menu-web/src/main/java/com/liferay/accessibility/menu/web/internal/servlet/taglib/AccessibilityMenuBodyTopJSPDynamicInclude.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.accessibility.menu.web.internal.servlet.taglib;
+
+import com.liferay.accessibility.menu.web.internal.constants.AccessibilityMenuPortletKeys;
+import com.liferay.petra.reflect.ReflectionUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import com.liferay.taglib.portletext.RuntimeTag;
+import com.liferay.taglib.servlet.PageContextFactoryUtil;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Evan Thibodeau
+ */
+@Component(service = DynamicInclude.class)
+public class AccessibilityMenuBodyTopJSPDynamicInclude
+	extends BaseDynamicInclude {
+
+	@Override
+	public void include(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
+		throws IOException {
+
+		PageContext pageContext = PageContextFactoryUtil.create(
+			httpServletRequest, httpServletResponse);
+
+		try {
+			JspWriter jspWriter = pageContext.getOut();
+
+			jspWriter.write(
+				"<div class=\"accessibility-menu\" style=\"display: none;\">");
+
+			RuntimeTag runtimeTag = new RuntimeTag();
+
+			runtimeTag.setPortletName(
+				AccessibilityMenuPortletKeys.ACCESSIBILITY_MENU);
+
+			runtimeTag.doTag(pageContext);
+
+			jspWriter.write("</div>");
+		}
+		catch (Exception exception) {
+			ReflectionUtil.throwException(exception);
+		}
+	}
+
+	@Override
+	public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+		dynamicIncludeRegistry.register(
+			"/html/common/themes/body_top.jsp#post");
+	}
+
+}
