f93205ba70f1ba053aa9c174fe3698f74f17cef6
Marco Galluzzi <marco.galluzzi@liferay.com>
2023-06-09 12:31:24 +0100

    LPS-186422 Refactor - change order by column in proper place if "recent" filter has been selected

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminDisplayContext.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLAdminDisplayContext.java
@@ -215,6 +215,10 @@ public class DLAdminDisplayContext {
 			return _orderByCol;
 		}
 
+		if (isNavigationRecent()) {
+			return "modifiedDate";
+		}
+
 		String orderByCol = ParamUtil.getString(
 			_httpServletRequest, "orderByCol");
 
@@ -686,11 +690,6 @@ public class DLAdminDisplayContext {
 			ArrayUtil.isNotEmpty(assetTagIds) ||
 			ArrayUtil.isNotEmpty(extensions) || navigation.equals("mine") ||
 			navigation.equals("recent")) {
-
-			if (navigation.equals("recent")) {
-				dlSearchContainer.setOrderByCol("modifiedDate");
-				dlSearchContainer.setOrderByType("desc");
-			}
 
 			SearchContext searchContext = _getSearchContext(dlSearchContainer);
 
