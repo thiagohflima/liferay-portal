97f595b40fe911fd90feef04b3bffe05aa34fdf5
shuyangzhou <shuyang.zhou@liferay.com>
2023-06-14 10:00:48 -0700

    LPS-188129 Refactor ModelListenerRegistrationUtil to use ServiceTrackerMap and sort ModelListeners by class name.

--- portal-kernel/src/com/liferay/portal/kernel/model/ModelListenerRegistrationUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/model/ModelListenerRegistrationUtil.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.model;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.bean.ClassLoaderBeanHandler;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
@@ -22,17 +24,12 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * @author Peter Fellwock
@@ -40,7 +37,8 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 public class ModelListenerRegistrationUtil {
 
 	public static <T> ModelListener<T>[] getModelListeners(Class<T> clazz) {
-		List<ModelListener<?>> modelListeners = _modelListeners.get(clazz);
+		List<ModelListener<?>> modelListeners = _modelListeners.getService(
+			clazz.getName());
 
 		if (modelListeners == null) {
 			return new ModelListener[0];
@@ -54,7 +52,7 @@ public class ModelListenerRegistrationUtil {
 
 		ServiceRegistration<?> serviceRegistration =
 			_bundleContext.registerService(
-				ModelListener.class.getName(), modelListener, null);
+				ModelListener.class, modelListener, null);
 
 		_serviceRegistrations.put(clazz.getName(), serviceRegistration);
 	}
@@ -70,131 +68,87 @@ public class ModelListenerRegistrationUtil {
 		}
 	}
 
-	private static final BundleContext _bundleContext =
-		SystemBundleUtil.getBundleContext();
-	private static final ConcurrentMap<Class<?>, List<ModelListener<?>>>
-		_modelListeners = new ConcurrentHashMap<>();
-	private static final Map<String, ServiceRegistration<?>>
-		_serviceRegistrations = new ConcurrentHashMap<>();
-	private static final ServiceTracker<ModelListener<?>, ModelListener<?>>
-		_serviceTracker;
+	private static Class<?> _getGenericSuperType(Class<?> clazz) {
+		try {
+			ParameterizedType parameterizedType =
+				(ParameterizedType)clazz.getGenericSuperclass();
 
-	private static class ModelListenerTrackerCustomizer
-		implements ServiceTrackerCustomizer
-			<ModelListener<?>, ModelListener<?>> {
+			Type[] types = parameterizedType.getActualTypeArguments();
 
-		@Override
-		public ModelListener<?> addingService(
-			ServiceReference<ModelListener<?>> serviceReference) {
-
-			ModelListener<?> modelListener = _bundleContext.getService(
-				serviceReference);
-
-			Class<?> modelClass = _getModelClass(modelListener);
-
-			if (modelClass == null) {
-				return null;
-			}
-
-			List<ModelListener<?>> modelListeners = _modelListeners.get(
-				modelClass);
-
-			if (modelListeners == null) {
-				modelListeners = new ArrayList<>();
-
-				List<ModelListener<?>> previousModelListeners =
-					_modelListeners.putIfAbsent(modelClass, modelListeners);
-
-				if (previousModelListeners != null) {
-					modelListeners = previousModelListeners;
-				}
-			}
-
-			modelListeners.add(modelListener);
-
-			return modelListener;
-		}
-
-		@Override
-		public void modifiedService(
-			ServiceReference<ModelListener<?>> serviceReference,
-			ModelListener<?> modelListener) {
-		}
-
-		@Override
-		public void removedService(
-			ServiceReference<ModelListener<?>> serviceReference,
-			ModelListener<?> modelListener) {
-
-			_bundleContext.ungetService(serviceReference);
-
-			Class<?> modelClass = _getModelClass(modelListener);
-
-			List<ModelListener<?>> modelListeners = _modelListeners.get(
-				modelClass);
-
-			if (modelListeners != null) {
-				modelListeners.remove(modelListener);
-
-				if (modelListeners.isEmpty()) {
-					_modelListeners.remove(modelClass);
-				}
+			if (types.length > 0) {
+				return (Class<?>)types[0];
 			}
 		}
-
-		private Class<?> _getGenericSuperType(Class<?> clazz) {
-			try {
-				ParameterizedType parameterizedType =
-					(ParameterizedType)clazz.getGenericSuperclass();
-
-				Type[] types = parameterizedType.getActualTypeArguments();
-
-				if (types.length > 0) {
-					return (Class<?>)types[0];
-				}
-			}
-			catch (Throwable throwable) {
-			}
-
-			return null;
+		catch (Throwable throwable) {
 		}
 
-		private Class<?> _getModelClass(ModelListener<?> modelListener) {
-			Class<?> clazz = modelListener.getModelClass();
-
-			if (clazz != null) {
-				return clazz;
-			}
-
-			clazz = modelListener.getClass();
-
-			if (ProxyUtil.isProxyClass(clazz)) {
-				InvocationHandler invocationHandler =
-					ProxyUtil.getInvocationHandler(modelListener);
-
-				if (invocationHandler instanceof ClassLoaderBeanHandler) {
-					ClassLoaderBeanHandler classLoaderBeanHandler =
-						(ClassLoaderBeanHandler)invocationHandler;
-
-					Object bean = classLoaderBeanHandler.getBean();
-
-					clazz = bean.getClass();
-				}
-			}
-
-			return _getGenericSuperType(clazz);
-		}
-
+		return null;
 	}
 
-	static {
-		_serviceTracker = new ServiceTracker<>(
-			_bundleContext,
-			SystemBundleUtil.createFilter(
-				"(objectClass=" + ModelListener.class.getName() + ")"),
-			new ModelListenerTrackerCustomizer());
+	private static Class<?> _getModelClass(ModelListener<?> modelListener) {
+		Class<?> clazz = modelListener.getModelClass();
 
-		_serviceTracker.open();
+		if (clazz != null) {
+			return clazz;
+		}
+
+		clazz = modelListener.getClass();
+
+		if (ProxyUtil.isProxyClass(clazz)) {
+			InvocationHandler invocationHandler =
+				ProxyUtil.getInvocationHandler(modelListener);
+
+			if (invocationHandler instanceof ClassLoaderBeanHandler) {
+				ClassLoaderBeanHandler classLoaderBeanHandler =
+					(ClassLoaderBeanHandler)invocationHandler;
+
+				Object bean = classLoaderBeanHandler.getBean();
+
+				clazz = bean.getClass();
+			}
+		}
+
+		return _getGenericSuperType(clazz);
+	}
+
+	private static final BundleContext _bundleContext =
+		SystemBundleUtil.getBundleContext();
+	private static final ServiceTrackerMap<String, List<ModelListener<?>>>
+		_modelListeners;
+	private static final Map<String, ServiceRegistration<?>>
+		_serviceRegistrations = new ConcurrentHashMap<>();
+
+	static {
+		_modelListeners = ServiceTrackerMapFactory.openMultiValueMap(
+			_bundleContext,
+			(Class<ModelListener<?>>)(Class<?>)ModelListener.class, null,
+			(serviceReference, emitter) -> {
+				ModelListener<?> modelListener = _bundleContext.getService(
+					serviceReference);
+
+				Class<?> modelClass = _getModelClass(modelListener);
+
+				if (modelClass != null) {
+					emitter.emit(modelClass.getName());
+				}
+			},
+			(serviceReference1, serviceReference2) -> {
+				ModelListener<?> modelListener1 = _bundleContext.getService(
+					serviceReference1);
+
+				Class<?> clazz1 = modelListener1.getClass();
+
+				String name1 = clazz1.getName();
+
+				ModelListener<?> modelListener2 = _bundleContext.getService(
+					serviceReference2);
+
+				Class<?> clazz2 = modelListener2.getClass();
+
+				String name2 = clazz2.getName();
+
+				return name1.compareTo(name2);
+			});
 	}
 
 }
