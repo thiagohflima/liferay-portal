a8f3d33b37a47bcf2b2738ecd0fdf6afc5f45f79
Danny Situ <danny.situ@liferay.com>
2023-06-15 12:34:41 -0700

    COMMERCE-11161 Use default company for search test

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/search/test/CPDefinitionIndexerTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/search/test/CPDefinitionIndexerTest.java
@@ -20,9 +20,7 @@ import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CommerceCatalog;
 import com.liferay.commerce.product.service.CommerceCatalogLocalService;
 import com.liferay.commerce.product.test.util.CPTestUtil;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Hits;
@@ -31,11 +29,9 @@ import com.liferay.portal.kernel.search.IndexerRegistry;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
-import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.search.test.util.HitsAssert;
 import com.liferay.portal.test.rule.Inject;
@@ -44,7 +40,6 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -63,19 +58,11 @@ public class CPDefinitionIndexerTest {
 			new LiferayIntegrationTestRule(),
 			PermissionCheckerMethodTestRule.INSTANCE);
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-		_company = CompanyTestUtil.addCompany();
-
-		_user = UserTestUtil.addUser(_company);
-
-		_indexer = _indexerRegistry.getIndexer(CPDefinition.class);
-	}
-
 	@Before
 	public void setUp() throws Exception {
-		_group = GroupTestUtil.addGroup(
-			_company.getCompanyId(), _user.getUserId(), 0);
+		_group = GroupTestUtil.addGroup();
+
+		_indexer = _indexerRegistry.getIndexer(CPDefinition.class);
 	}
 
 	@Test
@@ -108,13 +95,10 @@ public class CPDefinitionIndexerTest {
 			document.get(Field.ENTRY_CLASS_PK));
 	}
 
-	private static Company _company;
 	private static Indexer<CPDefinition> _indexer;
 
 	@Inject
 	private static IndexerRegistry _indexerRegistry;
-
-	private static User _user;
 
 	@Inject
 	private CommerceCatalogLocalService _commerceCatalogLocalService;
