596a0ecb6e2d284832239d4406fed4e05efcb29b
Brian Chan <brian.chan@liferay.com>
2023-06-15 16:21:52 +0530

    LPS-180450 Wordsmith

--- modules/util/portal-tools-db-upgrade-client/scripts/db_upgrade.sh
+++ modules/util/portal-tools-db-upgrade-client/scripts/db_upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "This script is deprecated. Please use db_upgrade_client.sh in the future."
+echo "This script is deprecated and replaced by db_upgrade_client.sh."
 
 ./db_upgrade_client.sh "$@"
