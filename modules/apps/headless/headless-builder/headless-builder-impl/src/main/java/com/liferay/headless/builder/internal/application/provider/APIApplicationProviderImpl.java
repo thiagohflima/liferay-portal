0cd3a87a6821c05fac16fd6f450c0408cce12b7b
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-06-16 23:31:38 +0200

    LPS-186786 Adding provider implementation consuming the MSODs

---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/application/provider/APIApplicationProviderImpl.java
@@ -0,0 +1,286 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.builder.internal.application.provider;
+
+import com.liferay.headless.builder.application.APIApplication;
+import com.liferay.headless.builder.application.provider.APIApplicationProvider;
+import com.liferay.headless.builder.internal.helper.ObjectEntryHelper;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.rest.dto.v1_0.ListEntry;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luis Miguel Barcos
+ * @authot Carlos Correa
+ * @author Alejandro Tardín
+ */
+@Component(service = APIApplicationProvider.class)
+public class APIApplicationProviderImpl implements APIApplicationProvider {
+
+	@Override
+	public APIApplication getAPIApplication(String baseURL, long companyId)
+		throws Exception {
+
+		ObjectEntry objectEntry = _objectEntryHelper.getObjectEntry(
+			companyId, "baseURL eq '" + baseURL + "'", "MSOD_API_APPLICATION");
+
+		if (objectEntry == null) {
+			return null;
+		}
+
+		List<APIApplication.Schema> schemas = _getSchemas(
+			objectEntry, companyId);
+
+		List<APIApplication.Endpoint> endpoints = _getEndpoints(
+			objectEntry.getExternalReferenceCode(), companyId, schemas);
+
+		Map<String, Object> properties = objectEntry.getProperties();
+
+		return new APIApplication() {
+
+			@Override
+			public String getBaseURL() {
+				return (String)properties.get("baseURL");
+			}
+
+			@Override
+			public long getCompanyId() {
+				return companyId;
+			}
+
+			@Override
+			public String getDescription() {
+				return (String)properties.get("description");
+			}
+
+			@Override
+			public List<Endpoint> getEndpoints() {
+				return endpoints;
+			}
+
+			@Override
+			public String getOsgiJaxRsName() {
+				return getTitle() + getCompanyId();
+			}
+
+			@Override
+			public List<Schema> getSchemas() {
+				return schemas;
+			}
+
+			@Override
+			public String getTitle() {
+				return (String)properties.get("title");
+			}
+
+			@Override
+			public String getVersion() {
+				return (String)properties.get("version");
+			}
+
+		};
+	}
+
+	private List<APIApplication.Endpoint> _getEndpoints(
+			String apiApplicationExternalReferenceCode, long companyId,
+			List<APIApplication.Schema> schemas)
+		throws Exception {
+
+		return TransformUtil.transform(
+			_objectEntryHelper.getObjectEntries(
+				companyId,
+				"apiApplicationToAPIEndpoints/externalReferenceCode eq '" +
+					apiApplicationExternalReferenceCode + "'",
+				"MSOD_API_ENDPOINT"),
+			objectEntry -> {
+				Map<String, Object> properties = objectEntry.getProperties();
+
+				return new APIApplication.Endpoint() {
+
+					@Override
+					public Http.Method getMethod() {
+						ListEntry listEntry = (ListEntry)properties.get(
+							"httpMethod");
+
+						return Http.Method.valueOf(
+							StringUtil.toUpperCase(listEntry.getKey()));
+					}
+
+					@Override
+					public String getPath() {
+						return (String)properties.get("path");
+					}
+
+					@Override
+					public APIApplication.Schema getRequestSchema() {
+						return _getSchema(
+							(String)properties.get(
+								"r_requestAPISchemaToAPIEndpoints_c_" +
+									"apiSchemaERC"),
+							schemas);
+					}
+
+					@Override
+					public APIApplication.Schema getResponseSchema() {
+						return _getSchema(
+							(String)properties.get(
+								"r_responseAPISchemaToAPIEndpoints_c_" +
+									"apiSchemaERC"),
+							schemas);
+					}
+
+					@Override
+					public Scope getScope() {
+						ListEntry listEntry = (ListEntry)properties.get(
+							"scope");
+
+						return Scope.valueOf(
+							StringUtil.toUpperCase(listEntry.getKey()));
+					}
+
+				};
+			});
+	}
+
+	private List<APIApplication.Property> _getProperties(
+		Map<String, Object> schemaProperties, long companyId) {
+
+		return TransformUtil.transformToList(
+			(ObjectEntry[])schemaProperties.get("apiSchemaToAPIProperties"),
+			propertyObjectEntry -> {
+				Map<String, Object> properties =
+					propertyObjectEntry.getProperties();
+
+				String mainObjectDefinitionERC = MapUtil.getString(
+					schemaProperties, "mainObjectDefinitionERC");
+
+				properties.put(
+					"mainObjectDefinitionERC", mainObjectDefinitionERC);
+
+				ObjectDefinition objectDefinition =
+					_objectDefinitionLocalService.
+						getObjectDefinitionByExternalReferenceCode(
+							mainObjectDefinitionERC, companyId);
+
+				ObjectField objectField =
+					_objectFieldLocalService.getObjectField(
+						(String)properties.get("objectFieldERC"),
+						objectDefinition.getObjectDefinitionId());
+
+				return new APIApplication.Property() {
+
+					@Override
+					public String getDescription() {
+						return (String)properties.get("description");
+					}
+
+					@Override
+					public String getName() {
+						return (String)properties.get("name");
+					}
+
+					@Override
+					public String getType() {
+						return objectField.getBusinessType();
+					}
+
+				};
+			});
+	}
+
+	private APIApplication.Schema _getSchema(
+		String externalReferenceCode, List<APIApplication.Schema> schemas) {
+
+		if (Validator.isBlank(externalReferenceCode)) {
+			return null;
+		}
+
+		for (APIApplication.Schema schema : schemas) {
+			if (StringUtil.equals(
+					schema.getExternalReferenceCode(), externalReferenceCode)) {
+
+				return schema;
+			}
+		}
+
+		throw new IllegalStateException(
+			"The schema with external reference code " + externalReferenceCode +
+				" is not defined");
+	}
+
+	private List<APIApplication.Schema> _getSchemas(
+			ObjectEntry apiApplicationObjectEntry, long companyId)
+		throws Exception {
+
+		return TransformUtil.transform(
+			_objectEntryHelper.getObjectEntries(
+				companyId,
+				"apiApplicationToAPISchemas/externalReferenceCode eq '" +
+					apiApplicationObjectEntry.getExternalReferenceCode() + "'",
+				Arrays.asList("apiSchemaToAPIProperties"), "MSOD_API_SCHEMA"),
+			objectEntry -> {
+				Map<String, Object> properties = objectEntry.getProperties();
+
+				return new APIApplication.Schema() {
+
+					@Override
+					public String getDescription() {
+						return (String)properties.get("description");
+					}
+
+					@Override
+					public String getExternalReferenceCode() {
+						return objectEntry.getExternalReferenceCode();
+					}
+
+					@Override
+					public String getName() {
+						return (String)properties.get("name");
+					}
+
+					@Override
+					public List<APIApplication.Property> getProperties() {
+						return _getProperties(properties, companyId);
+					}
+
+				};
+			});
+	}
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryHelper _objectEntryHelper;
+
+	@Reference
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+}
---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/helper/ObjectEntryHelper.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.builder.internal.helper;
+
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.fields.NestedFieldsContext;
+import com.liferay.portal.vulcan.fields.NestedFieldsContextThreadLocal;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luis Miguel Barcos
+ * @authot Carlos Correa
+ * @author Alejandro Tardín
+ */
+@Component(service = ObjectEntryHelper.class)
+public class ObjectEntryHelper {
+
+	public List<ObjectEntry> getObjectEntries(
+			long companyId, String filterString, List<String> nestedFields,
+			String objectDefinitionExternalReferenceCode)
+		throws Exception {
+
+		NestedFieldsContext nestedFieldsContext = new NestedFieldsContext(
+			1, nestedFields);
+
+		NestedFieldsContext oldNestedFieldsContext =
+			NestedFieldsContextThreadLocal.getAndSetNestedFieldsContext(
+				nestedFieldsContext);
+
+		try {
+			return getObjectEntries(
+				companyId, filterString, objectDefinitionExternalReferenceCode);
+		}
+		finally {
+			NestedFieldsContextThreadLocal.setNestedFieldsContext(
+				oldNestedFieldsContext);
+		}
+	}
+
+	public List<ObjectEntry> getObjectEntries(
+			long companyId, String filterString,
+			String objectDefinitionExternalReferenceCode)
+		throws Exception {
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				getObjectDefinitionByExternalReferenceCode(
+					objectDefinitionExternalReferenceCode, companyId);
+
+		PermissionThreadLocal.setPermissionChecker(
+			_permissionCheckerFactory.create(
+				_userLocalService.getUser(objectDefinition.getUserId())));
+
+		Page<ObjectEntry> page = _objectEntryManager.getObjectEntries(
+			companyId, objectDefinition, null, null,
+			new DefaultDTOConverterContext(
+				false, null, null, null, null, LocaleUtil.getSiteDefault(),
+				null, _userLocalService.getUser(objectDefinition.getUserId())),
+			filterString, Pagination.of(QueryUtil.ALL_POS, QueryUtil.ALL_POS),
+			null, null);
+
+		return new ArrayList<>(page.getItems());
+	}
+
+	public ObjectEntry getObjectEntry(
+			long companyId, String filterString,
+			String objectDefinitionExternalReferenceCode)
+		throws Exception {
+
+		List<ObjectEntry> objectEntries = getObjectEntries(
+			companyId, filterString, objectDefinitionExternalReferenceCode);
+
+		if (ListUtil.isEmpty(objectEntries)) {
+			return null;
+		}
+
+		return objectEntries.get(0);
+	}
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference(target = "(object.entry.manager.storage.type=default)")
+	private ObjectEntryManager _objectEntryManager;
+
+	@Reference
+	private PermissionCheckerFactory _permissionCheckerFactory;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
