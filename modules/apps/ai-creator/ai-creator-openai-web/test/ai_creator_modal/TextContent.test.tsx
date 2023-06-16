cdf40be91c844c3926669a26b24efaed2a4f5362
Pablo Molina <contact@pablomolina.me>
2023-06-16 11:45:19 +0200

    LPS-182830 Add tests for TextContent

---
+++ modules/apps/ai-creator/ai-creator-openai-web/test/ai_creator_modal/TextContent.test.tsx
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import {TextContent} from '../../src/main/resources/META-INF/resources/ai_creator_modal/TextContent';
+
+describe('TextContent', () => {
+	it('shows the given content inside a readonly input', () => {
+		render(
+			<TextContent
+				content="Sample content"
+				portletNamespace="namespace"
+			/>
+		);
+
+		const input = screen.getByLabelText('content');
+
+		expect(input).toHaveAttribute('readonly');
+		expect(input).toHaveValue('Sample content');
+	});
+});
