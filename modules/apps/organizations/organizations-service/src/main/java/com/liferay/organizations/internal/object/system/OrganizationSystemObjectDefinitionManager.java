3f24fa45841f65ca99f8cf4feac8ffb44b15c29d
Mateus Santana <mateus.santana@liferay.com>
2023-06-16 10:08:54 -0300

    LPS-186330 Add OrganizationSystemObjectDefinitionManager class

---
+++ modules/apps/organizations/organizations-service/src/main/java/com/liferay/organizations/internal/object/system/OrganizationSystemObjectDefinitionManager.java
@@ -0,0 +1,217 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.organizations.internal.object.system;
+
+import com.liferay.headless.admin.user.dto.v1_0.Organization;
+import com.liferay.headless.admin.user.resource.v1_0.OrganizationResource;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.system.BaseSystemObjectDefinitionManager;
+import com.liferay.object.system.JaxRsApplicationDescriptor;
+import com.liferay.object.system.SystemObjectDefinitionManager;
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.Table;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.OrganizationTable;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mateus Santana
+ */
+@Component(service = SystemObjectDefinitionManager.class)
+public class OrganizationSystemObjectDefinitionManager
+	extends BaseSystemObjectDefinitionManager {
+
+	@Override
+	public long addBaseModel(User user, Map<String, Object> values)
+		throws Exception {
+
+		OrganizationResource organizationResource = _buildOrganizationResource(
+			user);
+
+		Organization organization = organizationResource.postOrganization(
+			_toOrganization(values));
+
+		setExtendedProperties(
+			Organization.class.getName(), organization, user, values);
+
+		return GetterUtil.getLong(organization.getId());
+	}
+
+	@Override
+	public BaseModel<?> deleteBaseModel(BaseModel<?> baseModel)
+		throws PortalException {
+
+		return _organizationLocalService.deleteOrganization(
+			(com.liferay.portal.kernel.model.Organization)baseModel);
+	}
+
+	@Override
+	public BaseModel<?> fetchBaseModelByExternalReferenceCode(
+		String externalReferenceCode, long companyId) {
+
+		return _organizationLocalService.
+			fetchOrganizationByExternalReferenceCode(
+				externalReferenceCode, companyId);
+	}
+
+	@Override
+	public BaseModel<?> getBaseModelByExternalReferenceCode(
+			String externalReferenceCode, long companyId)
+		throws PortalException {
+
+		return _organizationLocalService.getOrganizationByExternalReferenceCode(
+			externalReferenceCode, companyId);
+	}
+
+	@Override
+	public String getExternalReferenceCode(long primaryKey)
+		throws PortalException {
+
+		com.liferay.portal.kernel.model.Organization organization =
+			_organizationLocalService.getOrganization(primaryKey);
+
+		return organization.getExternalReferenceCode();
+	}
+
+	@Override
+	public JaxRsApplicationDescriptor getJaxRsApplicationDescriptor() {
+		return new JaxRsApplicationDescriptor(
+			"Liferay.Headless.Admin.User", "headless-admin-user",
+			"organizations", "v1.0");
+	}
+
+	@Override
+	public Map<Locale, String> getLabelMap() {
+		return createLabelMap("organization");
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return com.liferay.portal.kernel.model.Organization.class;
+	}
+
+	@Override
+	public List<ObjectField> getObjectFields() {
+		return Arrays.asList(
+			createObjectField(
+				"Text", "comments", "String", "comments", "comments", false,
+				true),
+			createObjectField(
+				"Text", "name", "String", "name", "name", true, true));
+	}
+
+	@Override
+	public Map<Locale, String> getPluralLabelMap() {
+		return createLabelMap("organizations");
+	}
+
+	@Override
+	public Column<?, Long> getPrimaryKeyColumn() {
+		return OrganizationTable.INSTANCE.organizationId;
+	}
+
+	@Override
+	public String getScope() {
+		return ObjectDefinitionConstants.SCOPE_COMPANY;
+	}
+
+	@Override
+	public Table getTable() {
+		return OrganizationTable.INSTANCE;
+	}
+
+	@Override
+	public String getTitleObjectFieldName() {
+		return "name";
+	}
+
+	@Override
+	public Map<String, Object> getVariables(
+		String contentType, ObjectDefinition objectDefinition,
+		boolean oldValues, JSONObject payloadJSONObject) {
+
+		Map<String, Object> variables = super.getVariables(
+			contentType, objectDefinition, oldValues, payloadJSONObject);
+
+		if (variables.containsKey("comments")) {
+			variables.put("comment", variables.get("comments"));
+		}
+
+		return variables;
+	}
+
+	@Override
+	public int getVersion() {
+		return 1;
+	}
+
+	@Override
+	public void updateBaseModel(
+			long primaryKey, User user, Map<String, Object> values)
+		throws Exception {
+
+		OrganizationResource organizationResource = _buildOrganizationResource(
+			user);
+
+		Organization organization = organizationResource.patchOrganization(
+			String.valueOf(primaryKey), _toOrganization(values));
+
+		setExtendedProperties(
+			Organization.class.getName(), organization, user, values);
+	}
+
+	private OrganizationResource _buildOrganizationResource(User user) {
+		OrganizationResource.Builder builder =
+			_organizationResourceFactory.create();
+
+		return builder.checkPermissions(
+			false
+		).preferredLocale(
+			user.getLocale()
+		).user(
+			user
+		).build();
+	}
+
+	private Organization _toOrganization(Map<String, Object> values) {
+		return new Organization() {
+			{
+				comment = GetterUtil.getString(values.get("comment"));
+				name = GetterUtil.getString(values.get("name"));
+			}
+		};
+	}
+
+	@Reference
+	private OrganizationLocalService _organizationLocalService;
+
+	@Reference
+	private OrganizationResource.Factory _organizationResourceFactory;
+
+}
