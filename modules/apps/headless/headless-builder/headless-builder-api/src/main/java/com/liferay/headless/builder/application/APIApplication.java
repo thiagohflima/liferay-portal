6ff7c731d89a8838c0176a4ede934ceb934cb803
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-06-16 21:21:40 +0200

    LPS-186786 Adding public interfaces

---
+++ modules/apps/headless/headless-builder/headless-builder-api/src/main/java/com/liferay/headless/builder/application/APIApplication.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.builder.application;
+
+import com.liferay.portal.kernel.util.Http;
+
+import java.util.List;
+
+/**
+ * @author Alejandro Tardín
+ */
+public interface APIApplication {
+
+	public String getBaseURL();
+
+	public long getCompanyId();
+
+	public String getDescription();
+
+	public List<Endpoint> getEndpoints();
+
+	public String getOsgiJaxRsName();
+
+	public List<Schema> getSchemas();
+
+	public String getTitle();
+
+	public String getVersion();
+
+	public interface Endpoint {
+
+		public Http.Method getMethod();
+
+		public String getPath();
+
+		public Schema getRequestSchema();
+
+		public Schema getResponseSchema();
+
+		public Scope getScope();
+
+		public enum Scope {
+
+			COMPANY, GROUP
+
+		}
+
+	}
+
+	public interface Property {
+
+		public String getDescription();
+
+		public String getName();
+
+		public String getType();
+
+	}
+
+	public interface Schema {
+
+		public String getDescription();
+
+		public String getExternalReferenceCode();
+
+		public String getName();
+
+		public List<Property> getProperties();
+
+	}
+
+}
