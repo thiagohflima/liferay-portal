4875096517d24f63a552209ab86a37f81908a260
Bruno Santana <bruno.santana@liferay.com>
2023-06-19 16:48:04 -0300

    LPS-187212 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFRequestList/utils/getMDFBudgetInfos.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFRequestList/utils/getMDFBudgetInfos.ts
@@ -17,7 +17,7 @@ export default function getMDFBudgetInfos(
 	totalCostOfExpense?: number,
 	totalRequested?: number,
 	currency?: LiferayPicklist,
-	requestStatus?: string,
+	requestStatus?: string
 ) {
 	if (totalCostOfExpense && totalRequested) {
 		return {
@@ -30,7 +30,7 @@ export default function getMDFBudgetInfos(
 			[MDFColumnKey.APPROVED]:
 				requestStatus === 'approved'
 					? getIntlNumberFormat(currency).format(totalRequested)
-					: '-'
+					: '-',
 		};
 	}
 }
