99d08f2f5c97b521ea57af51b03ef4997dc3e210
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-19 08:22:01 +0200

    LPS-188018 Adds a mock implementation for AICreatorOpenAIClient in order to provide a way to run manual and POSHI tests without interact with the real API.
    We are providing a key that is considered as a valid API key and other keys that could be used to simulate the different handled errors.
    
    The error keys provided are:
    
    OPENAI_API_INVALID_API_KEY --> the response will be the same as when the real API returns invalid API key's response.
    OPENAI_API_IOEXCEPTION --> the response will be the same as when the real API throws an exception.
    OPENAI_API_{0}_RESPONSE_CODE --> the response will be the same as when the real API response code is {0}.

---
+++ modules/apps/ai-creator/ai-creator-openai-web/src/main/java/com/liferay/ai/creator/openai/web/internal/client/MockAICreatorOpenAIClient.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ai.creator.openai.web.internal.client;
+
+import com.liferay.ai.creator.openai.web.internal.exception.AICreatorOpenAIClientException;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.io.IOException;
+
+import java.net.HttpURLConnection;
+
+import java.util.Locale;
+import java.util.Objects;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = AICreatorOpenAIClient.class)
+public class MockAICreatorOpenAIClient implements AICreatorOpenAIClient {
+
+	@Override
+	public String getCompletion(
+		String apiKey, String content, Locale locale, String tone, int words) {
+
+		if (Objects.equals(apiKey, "VALID_API_KEY")) {
+			return "OPENAI_API_COMPLETION_RESPONSE_CONTENT";
+		}
+
+		throw _getAICreatorOpenAIClientException(apiKey);
+	}
+
+	@Override
+	public void validateAPIKey(String apiKey) {
+		if (Objects.equals(apiKey, "VALID_API_KEY")) {
+			return;
+		}
+
+		throw _getAICreatorOpenAIClientException(apiKey);
+	}
+
+	private AICreatorOpenAIClientException _getAICreatorOpenAIClientException(
+		String apiKey) {
+
+		if (Objects.equals(apiKey, "OPENAI_API_INVALID_API_KEY")) {
+			return new AICreatorOpenAIClientException(
+				"invalid_api_key", "invalid_api_key_message",
+				HttpURLConnection.HTTP_OK);
+		}
+
+		if (Objects.equals(apiKey, "OPENAI_API_IOEXCEPTION")) {
+			return new AICreatorOpenAIClientException(new IOException());
+		}
+
+		int responseCode = GetterUtil.getInteger(
+			StringUtils.substringBetween(
+				apiKey, "OPENAI_API_", "_RESPONSE_CODE"));
+
+		if (responseCode > 0) {
+			return new AICreatorOpenAIClientException(responseCode);
+		}
+
+		return new AICreatorOpenAIClientException(
+			new UnsupportedOperationException(
+				"Invalid API Key to use MockAICreatorOpenAIClient, API Key: " +
+					apiKey));
+	}
+
+}
