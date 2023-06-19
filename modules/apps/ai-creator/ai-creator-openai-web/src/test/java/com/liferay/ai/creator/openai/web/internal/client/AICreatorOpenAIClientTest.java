e102ef7851f405d614cc1767e7d6cefdf66abb06
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-06-19 08:21:25 +0200

    LPS-182829 Adds a suite of JUnit tests to full coverage the getCompletion method logic

--- modules/apps/ai-creator/ai-creator-openai-web/src/test/java/com/liferay/ai/creator/openai/web/internal/client/AICreatorOpenAIClientTest.java
+++ modules/apps/ai-creator/ai-creator-openai-web/src/test/java/com/liferay/ai/creator/openai/web/internal/client/AICreatorOpenAIClientTest.java
@@ -15,12 +15,19 @@
 package com.liferay.ai.creator.openai.web.internal.client;
 
 import com.liferay.ai.creator.openai.web.internal.exception.AICreatorOpenAIClientException;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.json.JSONObjectImpl;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
 
@@ -29,6 +36,9 @@ import java.io.InputStream;
 
 import java.net.HttpURLConnection;
 
+import java.util.Locale;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -51,6 +61,8 @@ public class AICreatorOpenAIClientTest {
 
 	@Before
 	public void setUp() throws IOException {
+		_originalLanguage = LanguageUtil.getLanguage();
+
 		_aiCreatorOpenAIClient = new AICreatorOpenAIClient();
 
 		_http = Mockito.mock(Http.class);
@@ -62,6 +74,156 @@ public class AICreatorOpenAIClientTest {
 
 		ReflectionTestUtil.setFieldValue(
 			_aiCreatorOpenAIClient, "_jsonFactory", _jsonFactory);
+	}
+
+	@After
+	public void tearDown() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(_originalLanguage);
+	}
+
+	@Test
+	public void testGetCompletion() throws Exception {
+		String messageContent = RandomTestUtil.randomString();
+
+		JSONObject responseJSONObject = JSONUtil.put(
+			"choices",
+			JSONUtil.put(
+				JSONUtil.put(
+					"message", JSONUtil.put("content", messageContent))));
+
+		Http.Response response = _getMockResponse(
+			HttpURLConnection.HTTP_OK, responseJSONObject);
+
+		_mockLanguage();
+
+		String apiKey = RandomTestUtil.randomString();
+		String content = RandomTestUtil.randomString();
+		String tone = RandomTestUtil.randomString();
+		int words = RandomTestUtil.randomInt();
+
+		Assert.assertEquals(
+			messageContent,
+			_aiCreatorOpenAIClient.getCompletion(
+				apiKey, content, LocaleUtil.getDefault(), tone, words));
+
+		_assertMessageRoleSystemContent(LocaleUtil.getDefault(), tone, words);
+
+		_assertOptions(
+			apiKey, content, ContentTypes.APPLICATION_JSON,
+			"https://api.openai.com/v1/chat/completions");
+
+		_assertResponse(response);
+	}
+
+	@Test
+	public void testGetCompletionIOException() throws Exception {
+		IOException ioException = new IOException();
+
+		Mockito.when(
+			_http.URLtoInputStream(Mockito.any(Http.Options.class))
+		).thenThrow(
+			ioException
+		);
+
+		_mockLanguage();
+
+		String apiKey = RandomTestUtil.randomString();
+		String content = RandomTestUtil.randomString();
+
+		try {
+			_aiCreatorOpenAIClient.getCompletion(
+				apiKey, content, LocaleUtil.getDefault(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomInt());
+
+			Assert.fail();
+		}
+		catch (AICreatorOpenAIClientException aiCreatorOpenAIClientException) {
+			Assert.assertEquals(
+				ioException, aiCreatorOpenAIClientException.getCause());
+		}
+
+		_assertOptions(
+			apiKey, content, ContentTypes.APPLICATION_JSON,
+			"https://api.openai.com/v1/chat/completions");
+	}
+
+	@Test
+	public void testGetCompletionResponseWithErrorKey() throws Exception {
+		JSONObject errorJSONObject = JSONUtil.put(
+			"code", RandomTestUtil.randomString()
+		).put(
+			"message", RandomTestUtil.randomString()
+		);
+
+		Http.Response response = _getMockResponse(
+			HttpURLConnection.HTTP_OK, JSONUtil.put("error", errorJSONObject));
+
+		_mockLanguage();
+
+		String apiKey = RandomTestUtil.randomString();
+		String content = RandomTestUtil.randomString();
+
+		try {
+			_aiCreatorOpenAIClient.getCompletion(
+				apiKey, content, LocaleUtil.getDefault(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomInt());
+
+			Assert.fail();
+		}
+		catch (AICreatorOpenAIClientException aiCreatorOpenAIClientException) {
+			Assert.assertEquals(
+				errorJSONObject.getString("code"),
+				aiCreatorOpenAIClientException.getCode());
+			Assert.assertEquals(
+				errorJSONObject.getString("message"),
+				aiCreatorOpenAIClientException.getMessage());
+
+			Assert.assertEquals(
+				HttpURLConnection.HTTP_OK,
+				aiCreatorOpenAIClientException.getResponseCode());
+		}
+
+		_assertOptions(
+			apiKey, content, ContentTypes.APPLICATION_JSON,
+			"https://api.openai.com/v1/chat/completions");
+
+		_assertResponse(response);
+	}
+
+	@Test
+	public void testGetCompletionUnauthorizedResponseCode() throws Exception {
+		JSONObject responseJSONObject = Mockito.mock(JSONObject.class);
+
+		Http.Response response = _getMockResponse(
+			HttpURLConnection.HTTP_UNAUTHORIZED, responseJSONObject);
+
+		_mockLanguage();
+
+		String apiKey = RandomTestUtil.randomString();
+		String content = RandomTestUtil.randomString();
+
+		try {
+			_aiCreatorOpenAIClient.getCompletion(
+				apiKey, content, LocaleUtil.getDefault(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomInt());
+
+			Assert.fail();
+		}
+		catch (AICreatorOpenAIClientException aiCreatorOpenAIClientException) {
+			Assert.assertEquals(
+				HttpURLConnection.HTTP_UNAUTHORIZED,
+				aiCreatorOpenAIClientException.getResponseCode());
+		}
+
+		_assertOptions(
+			apiKey, content, ContentTypes.APPLICATION_JSON,
+			"https://api.openai.com/v1/chat/completions");
+
+		_assertResponseJSONObject(responseJSONObject);
+
+		_assertResponse(response, Mockito.times(2));
 	}
 
 	@Test
@@ -186,7 +348,75 @@ public class AICreatorOpenAIClientTest {
 		).getResponseCode();
 	}
 
+	private void _assertBody(String content, Http.Body body) throws Exception {
+		Assert.assertNotNull(body);
+
+		Assert.assertEquals(
+			ContentTypes.APPLICATION_JSON, body.getContentType());
+		Assert.assertEquals(StringPool.UTF8, body.getCharset());
+
+		Assert.assertTrue(
+			body.getContent(), JSONUtil.isJSONObject(body.getContent()));
+
+		JSONObject contentJSONObject = new JSONObjectImpl(body.getContent());
+
+		Assert.assertEquals(
+			"gpt-3.5-turbo", contentJSONObject.getString("model"));
+
+		JSONArray messagesJSONArray = contentJSONObject.getJSONArray(
+			"messages");
+
+		Assert.assertNotNull(contentJSONObject.toString(), messagesJSONArray);
+
+		Assert.assertEquals(
+			messagesJSONArray.toString(), 2, messagesJSONArray.length());
+
+		JSONObject messageJSONObject1 = messagesJSONArray.getJSONObject(0);
+
+		Assert.assertEquals("system", messageJSONObject1.getString("role"));
+
+		JSONObject messageJSONObject2 = messagesJSONArray.getJSONObject(1);
+
+		Assert.assertEquals(content, messageJSONObject2.getString("content"));
+
+		Assert.assertEquals("user", messageJSONObject2.getString("role"));
+	}
+
+	private void _assertMessageRoleSystemContent(
+		Locale locale, String tone, int words) {
+
+		ArgumentCaptor<String[]> argumentCaptor = ArgumentCaptor.forClass(
+			String[].class);
+
+		Mockito.verify(
+			_language
+		).format(
+			Mockito.eq(locale),
+			Mockito.eq(
+				"i-want-you-to-create-a-text-using-x-as-the-language,-of-" +
+					"approximately-x-words,-and-using-a-x-tone"),
+			argumentCaptor.capture()
+		);
+
+		String[] arguments = argumentCaptor.getValue();
+
+		Assert.assertEquals(arguments.toString(), 3, arguments.length);
+
+		Assert.assertEquals(locale.getDisplayName(locale), arguments[0]);
+
+		Assert.assertEquals(String.valueOf(words), arguments[1]);
+
+		Assert.assertEquals(tone, arguments[2]);
+	}
+
 	private void _assertOptions(String apiKey, String location)
+		throws Exception {
+
+		_assertOptions(apiKey, null, null, location);
+	}
+
+	private void _assertOptions(
+			String apiKey, String content, String contentType, String location)
 		throws Exception {
 
 		ArgumentCaptor<Http.Options> argumentCaptor = ArgumentCaptor.forClass(
@@ -200,9 +430,17 @@ public class AICreatorOpenAIClientTest {
 
 		Http.Options options = argumentCaptor.getValue();
 
-		Assert.assertEquals(location, options.getLocation());
+		if (Validator.isNull(content)) {
+			Assert.assertNull(options.getBody());
+		}
+		else {
+			_assertBody(content, options.getBody());
+		}
+
 		Assert.assertEquals(
 			"Bearer " + apiKey, options.getHeader("Authorization"));
+		Assert.assertEquals(contentType, options.getHeader("Content-Type"));
+		Assert.assertEquals(location, options.getLocation());
 	}
 
 	private void _assertResponseJSONObject(JSONObject responseJSONObject) {
@@ -262,8 +500,28 @@ public class AICreatorOpenAIClientTest {
 		return response;
 	}
 
+	private void _mockLanguage() {
+		_language = Mockito.mock(Language.class);
+
+		Mockito.when(
+			_language.get(Mockito.any(Locale.class), Mockito.anyString())
+		).thenAnswer(
+			invocationOnMock -> invocationOnMock.getArgument(1, String.class)
+		);
+
+		ReflectionTestUtil.setFieldValue(
+			_aiCreatorOpenAIClient, "_language", _language);
+
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(_language);
+	}
+
+	private static Language _originalLanguage;
+
 	private AICreatorOpenAIClient _aiCreatorOpenAIClient;
 	private Http _http;
 	private JSONFactory _jsonFactory;
+	private Language _language;
 
 }
