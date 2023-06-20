32a93153f3897911f1bea4dcc9e7c88ff08fd4ae
Crescenzo Rega <crescenzo.rega@liferay.com>
2023-06-20 09:56:35 +0200

    COMMERCE-11699 Add permission logic to check inventory and catalog access for supplier

--- modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/internal/permission/CommerceInventoryWarehousePermissionImpl.java
+++ modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/internal/permission/CommerceInventoryWarehousePermissionImpl.java
@@ -14,17 +14,33 @@
 
 package com.liferay.commerce.inventory.internal.permission;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.constants.AccountRoleConstants;
+import com.liferay.account.service.AccountEntryLocalService;
 import com.liferay.commerce.inventory.constants.CommerceInventoryActionKeys;
 import com.liferay.commerce.inventory.constants.CommerceInventoryConstants;
 import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
 import com.liferay.commerce.inventory.permission.CommerceInventoryWarehousePermission;
 import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseLocalService;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.model.CommerceChannelRel;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.product.service.CommerceChannelRelLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.UserGroupRoleLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -137,7 +153,9 @@ public class CommerceInventoryWarehousePermissionImpl
 			permissionChecker.isOmniadmin() ||
 			_portletResourcePermission.contains(
 				PermissionThreadLocal.getPermissionChecker(), null,
-				CommerceInventoryActionKeys.MANAGE_INVENTORY)) {
+				CommerceInventoryActionKeys.MANAGE_INVENTORY) ||
+			_hasSupplierPermission(
+				permissionChecker, commerceInventoryWarehouse, actionId)) {
 
 			return true;
 		}
@@ -148,6 +166,91 @@ public class CommerceInventoryWarehousePermissionImpl
 			actionId);
 	}
 
+	private boolean _hasSupplierAccount(
+			PermissionChecker permissionChecker,
+			CommerceInventoryWarehouse commerceInventoryWarehouse)
+		throws PortalException {
+
+		List<CommerceChannel> accountEntryCommerceChannels =
+			TransformUtil.transform(
+				_accountEntryLocalService.getUserAccountEntries(
+					permissionChecker.getUserId(), 0L, StringPool.BLANK,
+					new String[] {AccountConstants.ACCOUNT_ENTRY_TYPE_SUPPLIER},
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS),
+				accountEntry -> {
+					if (_userGroupRoleLocalService.hasUserGroupRole(
+							permissionChecker.getUserId(),
+							accountEntry.getAccountEntryGroupId(),
+							AccountRoleConstants.ROLE_NAME_ACCOUNT_SUPPLIER)) {
+
+						List<CommerceChannel> commerceChannels =
+							_commerceChannelLocalService.
+								getCommerceChannelsByAccountEntryId(
+									accountEntry.getAccountEntryId());
+
+						if (ListUtil.isNotEmpty(commerceChannels)) {
+							return commerceChannels.get(0);
+						}
+					}
+
+					return null;
+				});
+
+		if (ListUtil.isEmpty(accountEntryCommerceChannels)) {
+			return false;
+		}
+
+		int commerceChannelRelsCount =
+			_commerceChannelRelLocalService.getCommerceChannelRelsCount(
+				CommerceInventoryWarehouse.class.getName(),
+				commerceInventoryWarehouse.getCommerceInventoryWarehouseId());
+
+		if (commerceChannelRelsCount == 0) {
+			return true;
+		}
+
+		for (CommerceChannel commerceChannel : accountEntryCommerceChannels) {
+			CommerceChannelRel commerceChannelRel =
+				_commerceChannelRelLocalService.fetchCommerceChannelRel(
+					CommerceInventoryWarehouse.class.getName(),
+					commerceInventoryWarehouse.
+						getCommerceInventoryWarehouseId(),
+					commerceChannel.getCommerceChannelId());
+
+			if (commerceChannelRel != null) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean _hasSupplierPermission(
+			PermissionChecker permissionChecker,
+			CommerceInventoryWarehouse commerceInventoryWarehouse,
+			String actionId)
+		throws PortalException {
+
+		if ((actionId.equals(ActionKeys.UPDATE) ||
+			 actionId.equals(ActionKeys.VIEW)) &&
+			_hasSupplierAccount(
+				permissionChecker, commerceInventoryWarehouse)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private AccountEntryLocalService _accountEntryLocalService;
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceChannelRelLocalService _commerceChannelRelLocalService;
+
 	@Reference
 	private CommerceInventoryWarehouseLocalService
 		_commerceInventoryWarehouseLocalService;
@@ -156,5 +259,11 @@ public class CommerceInventoryWarehousePermissionImpl
 		target = "(resource.name=" + CommerceInventoryConstants.RESOURCE_NAME + ")"
 	)
 	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private UserGroupRoleLocalService _userGroupRoleLocalService;
 
 }
