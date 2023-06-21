c294b10b7944341344698e9a581cc4e67823dac7
Richard Jeremias <ricmartinsrocha@gmail.com>
2023-06-21 20:26:14 -0300

    LPS-188388 Avoid accessing editable values on Fragments if empty

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment_content/FragmentContent.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment_content/FragmentContent.js
@@ -165,40 +165,44 @@ const FragmentContent = ({
 
 			Promise.all(
 				getAllEditables(fragmentElement).map((editable) => {
-					const editableValue =
-						editableValues[editable.editableValueNamespace][
-							editable.editableId
-						];
+					if (Object.keys(editableValues).length) {
+						const editableValue =
+							editableValues[editable.editableValueNamespace][
+								editable.editableId
+							];
 
-					return Promise.all([
-						resolveEditableValue(
-							editableValue,
-							languageId,
-							getFieldValue
-						),
-						resolveEditableConfig(
-							editableValue?.config || {},
-							languageId,
-							getFieldValue
-						),
-					]).then(([value, editableConfig]) => {
-						editable.processor.render(
-							editable.element,
-							value,
-							editableConfig,
-							languageId,
-							withinCollection
-						);
-
-						editable.element.classList.add('page-editor__editable');
-
-						if (TEXT_EDITABLE_TYPES.has(editable.type)) {
-							editable.element.setAttribute(
-								'data-tooltip-floating',
-								true
+						return Promise.all([
+							resolveEditableValue(
+								editableValue,
+								languageId,
+								getFieldValue
+							),
+							resolveEditableConfig(
+								editableValue?.config || {},
+								languageId,
+								getFieldValue
+							),
+						]).then(([value, editableConfig]) => {
+							editable.processor.render(
+								editable.element,
+								value,
+								editableConfig,
+								languageId,
+								withinCollection
 							);
-						}
-					});
+
+							editable.element.classList.add(
+								'page-editor__editable'
+							);
+
+							if (TEXT_EDITABLE_TYPES.has(editable.type)) {
+								editable.element.setAttribute(
+									'data-tooltip-floating',
+									true
+								);
+							}
+						});
+					}
 				})
 			).then(() => {
 				if (isMounted() && fragmentElement) {
