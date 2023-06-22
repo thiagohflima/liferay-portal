320562260fe1917ad396ce92c285335e1bfe8e95
Brian I. Kim <il.kim@liferay.com>
2023-06-22 10:09:15 -0700

    COMMERCE-11162 Reindex priceLists after updating catalog

--- modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/model/listener/CommerceCatalogModelListener.java
+++ modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/model/listener/CommerceCatalogModelListener.java
@@ -15,12 +15,19 @@
 package com.liferay.commerce.price.list.internal.model.listener;
 
 import com.liferay.commerce.price.list.internal.helper.CommerceBasePriceListHelper;
+import com.liferay.commerce.price.list.model.CommercePriceList;
+import com.liferay.commerce.price.list.service.CommercePriceListLocalService;
 import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -46,6 +53,21 @@ public class CommerceCatalogModelListener
 	}
 
 	@Override
+	public void onAfterUpdate(
+		CommerceCatalog originalCommerceCatalog,
+		CommerceCatalog commerceCatalog) {
+
+		try {
+			_reindexPriceLists(commerceCatalog);
+		}
+		catch (PortalException portalException) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(portalException);
+			}
+		}
+	}
+
+	@Override
 	public void onBeforeRemove(CommerceCatalog commerceCatalog) {
 		try {
 			_commerceBasePriceListHelper.deleteCatalogBaseCommercePriceList(
@@ -58,10 +80,34 @@ public class CommerceCatalogModelListener
 		}
 	}
 
+	private void _reindexPriceLists(CommerceCatalog commerceCatalog)
+		throws PortalException {
+
+		List<CommercePriceList> commercePriceLists =
+			_commercePriceListLocalService.getCommercePriceLists(
+				new long[] {commerceCatalog.getGroupId()},
+				commerceCatalog.getCompanyId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		if (!commercePriceLists.isEmpty()) {
+			Indexer<CommercePriceList> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(CommercePriceList.class);
+
+			for (CommercePriceList commercePriceList : commercePriceLists) {
+				indexer.reindex(
+					CommercePriceList.class.getName(),
+					commercePriceList.getCommercePriceListId());
+			}
+		}
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommerceCatalogModelListener.class);
 
 	@Reference
 	private CommerceBasePriceListHelper _commerceBasePriceListHelper;
+
+	@Reference
+	private CommercePriceListLocalService _commercePriceListLocalService;
 
 }
