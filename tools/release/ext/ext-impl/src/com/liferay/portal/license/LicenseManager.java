04f40646d3a9bd08d01e6d5a05b7b31ab1a8c8a4
Brian Chan <brian.chan@liferay.com>
2023-06-23 13:05:23 +0530

    LRP-4526 Fix for SF

--- tools/release/ext/ext-impl/src/com/liferay/portal/license/LicenseManager.java
+++ tools/release/ext/ext-impl/src/com/liferay/portal/license/LicenseManager.java
@@ -1241,7 +1241,7 @@ public class LicenseManager {
 			new ThreadFactory() {
 
 				public Thread newThread(Runnable runnable) {
-					Thread thread = new Thread(runnable, "");
+					Thread thread = new Thread(runnable, StringPool.BLANK);
 
 					thread.setDaemon(true);
 
