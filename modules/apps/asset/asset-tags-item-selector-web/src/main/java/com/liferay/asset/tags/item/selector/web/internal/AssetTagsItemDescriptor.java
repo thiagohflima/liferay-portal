c817f434d0b8c08dfb71b8dfea7c21080dd81472
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2023-06-29 00:14:21 +0200

    LPS-186527 Add TableItemView and ItemDescriptor for tags implementation

---
+++ modules/apps/asset/asset-tags-item-selector-web/src/main/java/com/liferay/asset/tags/item/selector/web/internal/AssetTagsItemDescriptor.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.item.selector.web.internal;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.tags.item.selector.web.internal.display.context.AssetTagsDisplayContext;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Stefan Tanasie
+ */
+public class AssetTagsItemDescriptor
+	implements ItemSelectorViewDescriptor.ItemDescriptor {
+
+	public AssetTagsItemDescriptor(
+		AssetTag assetTag, HttpServletRequest httpServletRequest,
+		AssetTagsDisplayContext assetTagsDisplayContext) {
+
+		_assetTag = assetTag;
+		_httpServletRequest = httpServletRequest;
+		_assetTagsDisplayContext = assetTagsDisplayContext;
+	}
+
+	@Override
+	public String getIcon() {
+		return null;
+	}
+
+	@Override
+	public String getImageURL() {
+		return null;
+	}
+
+	@Override
+	public String getPayload() {
+		if (_assetTagsDisplayContext.isMultiple()) {
+			return _assetTag.getName();
+		}
+
+		return JSONUtil.put(
+			"tagId", String.valueOf(_assetTag.getTagId())
+		).put(
+			"tagName", _assetTag.getName()
+		).toString();
+	}
+
+	@Override
+	public String getSubtitle(Locale locale) {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return HtmlUtil.escape(_assetTag.getName());
+	}
+
+	private final AssetTag _assetTag;
+	private final AssetTagsDisplayContext _assetTagsDisplayContext;
+	private final HttpServletRequest _httpServletRequest;
+
+}
---
+++ modules/apps/asset/asset-tags-item-selector-web/src/main/java/com/liferay/asset/tags/item/selector/web/internal/AssetTagsTableItemView.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.tags.item.selector.web.internal;
+
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.tags.item.selector.web.internal.display.context.AssetTagsDisplayContext;
+import com.liferay.item.selector.TableItemView;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.search.SearchEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.taglib.search.TextSearchEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Stefan Tanasie
+ */
+public class AssetTagsTableItemView implements TableItemView {
+
+	public AssetTagsTableItemView(
+		AssetTag assetTag, ThemeDisplay themeDisplay,
+		AssetTagsDisplayContext assetTagsDisplayContext) {
+
+		_assetTag = assetTag;
+		_themeDisplay = themeDisplay;
+		_assetTagsDisplayContext = assetTagsDisplayContext;
+	}
+
+	@Override
+	public List<String> getHeaderNames() {
+		if (_assetTagsDisplayContext.isMultiple()) {
+			return ListUtil.fromArray("name", "site");
+		}
+
+		return ListUtil.fromArray("name");
+	}
+
+	@Override
+	public List<SearchEntry> getSearchEntries(Locale locale) {
+		List<SearchEntry> searchEntries = new ArrayList<>();
+
+		TextSearchEntry nameTextSearchEntry = new TextSearchEntry();
+
+		nameTextSearchEntry.setName(HtmlUtil.escape(_assetTag.getName()));
+
+		String cssClass =
+			"entry entry-selector table-cell-expand-smaller table-cell-minw-80";
+
+		nameTextSearchEntry.setCssClass(cssClass);
+
+		nameTextSearchEntry.setData(
+			HashMapBuilder.<String, Object>put(
+				"entityid", _assetTag.getTagId()
+			).put(
+				"entityname", _assetTag.getName()
+			).build());
+
+		searchEntries.add(nameTextSearchEntry);
+
+		if (_assetTagsDisplayContext.isMultiple()) {
+			TextSearchEntry scopeTextSearchEntry = new TextSearchEntry();
+
+			scopeTextSearchEntry.setCssClass(
+				"table-cell-expand-smaller table-cell-minw-150");
+			scopeTextSearchEntry.setName(
+				LanguageUtil.get(
+					locale, HtmlUtil.escape(_getGroupDescriptiveName())));
+
+			searchEntries.add(scopeTextSearchEntry);
+		}
+
+		return searchEntries;
+	}
+
+	private String _getGroupDescriptiveName() {
+		try {
+			Group group = GroupLocalServiceUtil.fetchGroup(
+				_assetTag.getGroupId());
+
+			return group.getDescriptiveName(_themeDisplay.getLocale());
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AssetTagsTableItemView.class);
+
+	private final AssetTag _assetTag;
+	private final AssetTagsDisplayContext _assetTagsDisplayContext;
+	private final ThemeDisplay _themeDisplay;
+
+}
