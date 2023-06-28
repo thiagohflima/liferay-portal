665df0bce3117c21b649ca189aa6bebcae9369ec
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-06-28 10:48:49 +0800

    LPS-188902 Merge class OmniadminImpl into class OmniadminUtil to avoid using blocking ServiceProxyFactory

--- portal-impl/src/com/liferay/portlet/admin/util/OmniadminUtil.java
+++ portal-impl/src/com/liferay/portlet/admin/util/OmniadminUtil.java
@@ -14,9 +14,15 @@
 
 package com.liferay.portlet.admin.util;
 
-import com.liferay.admin.kernel.util.Omniadmin;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.util.PropsValues;
 
 /**
  * Provides utility methods for determining if a user is a universal
@@ -35,15 +41,67 @@ import com.liferay.portal.kernel.util.ServiceProxyFactory;
 public class OmniadminUtil {
 
 	public static boolean isOmniadmin(long userId) {
-		return _omniadmin.isOmniadmin(userId);
+		if (userId <= 0) {
+			return false;
+		}
+
+		try {
+			User user = UserLocalServiceUtil.fetchUser(userId);
+
+			if (user == null) {
+				return false;
+			}
+
+			return isOmniadmin(user);
+		}
+		catch (SystemException systemException) {
+
+			// LPS-52675
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(systemException);
+			}
+
+			return false;
+		}
 	}
 
 	public static boolean isOmniadmin(User user) {
-		return _omniadmin.isOmniadmin(user);
+		try {
+			if (PropsValues.OMNIADMIN_USERS.length > 0) {
+				for (int i = 0; i < PropsValues.OMNIADMIN_USERS.length; i++) {
+					if (PropsValues.OMNIADMIN_USERS[i] == user.getUserId()) {
+						if (user.getCompanyId() !=
+								PortalInstances.getDefaultCompanyId()) {
+
+							return false;
+						}
+
+						return true;
+					}
+				}
+
+				return false;
+			}
+
+			if (user.isGuestUser() ||
+				(user.getCompanyId() !=
+					PortalInstances.getDefaultCompanyId())) {
+
+				return false;
+			}
+
+			return RoleLocalServiceUtil.hasUserRole(
+				user.getUserId(), user.getCompanyId(),
+				RoleConstants.ADMINISTRATOR, true);
+		}
+		catch (Exception exception) {
+			_log.error("Unable to check if a user is an omniadmin", exception);
+
+			return false;
+		}
 	}
 
-	private static volatile Omniadmin _omniadmin =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			Omniadmin.class, OmniadminUtil.class, "_omniadmin", true);
+	private static final Log _log = LogFactoryUtil.getLog(OmniadminUtil.class);
 
 }
