001681c2cc5dc1eed978b176c4c44f64586ade21
Qi Zhang <qi.zhang@liferay.com>
2023-07-05 16:49:28 +0800

    LPS-156857 SF Remove unnecessary empty line

--- modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/portlet/action/EditImageConfigurationEntryMVCRenderCommand.java
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/portlet/action/EditImageConfigurationEntryMVCRenderCommand.java
@@ -70,7 +70,6 @@ public class EditImageConfigurationEntryMVCRenderCommand
 
 		renderRequest.setAttribute(
 			AMWebKeys.CONFIGURATION_ENTRY, amImageConfigurationEntry);
-
 		renderRequest.setAttribute(
 			AMWebKeys.CONFIGURATION_ENTRY_EDITABLE, configurationEntryEditable);
 
